b5a371189e89dd80d6b483a3d1ca6795d6e4c0df
Tomo3030 <40629469+Tomo3030@users.noreply.github.com>
2019-06-19 11:54:36 +0900

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyDlWmsImjqPgi3WuTr2CxT8h5d4I7owzX8",
-    authDomain: "figureout-functions.firebaseapp.com",
-    databaseURL: "https://figureout-functions.firebaseio.com",
-    projectId: "figureout-functions",
-    storageBucket: "figureout-functions.appspot.com",
-    messagingSenderId: "449774853896",
-    appId: "1:449774853896:web:5a428a9509b6bd38"
-  }
+
 };
 
 /*
