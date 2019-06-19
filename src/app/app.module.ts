f3629a839d646aba67fe3b5030761c9682600a5c
Tim Thompson <timt333@gmail.com>
2019-06-19 11:44:07 +0900

    base config with firebase and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { environment } from "src/environments/environment";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDlWmsImjqPgi3WuTr2CxT8h5d4I7owzX8",
+    authDomain: "figureout-functions.firebaseapp.com",
+    databaseURL: "https://figureout-functions.firebaseio.com",
+    projectId: "figureout-functions",
+    storageBucket: "figureout-functions.appspot.com",
+    messagingSenderId: "449774853896",
+    appId: "1:449774853896:web:5a428a9509b6bd38"
+  }
 };
 
 /*
